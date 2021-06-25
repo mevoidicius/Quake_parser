a7cccb6859610e2bbc6b1db2e0ae4d946502c325
mevoidicius <rostamlobolobo2@gmail.com>
2021-06-25 10:52:07 -0300

    {Classe que vai ter dados do Jogo}

--- src/Teste/base/Jogo.java
+++ src/Teste/base/Jogo.java
@@ -1,7 +1,36 @@
 package Teste.base;
-import Teste.base.Jogador;
+import Teste.base.Jogador;//utilizando dados da classe jogador
+import java.util.List;//listas de jogadores
+
 public class Jogo {
-    private
+    private String gamenumber;
+    private int allkills;
+    private List<Jogador> jogador;
 
 
+    public Jogo(String gamenumber,int allkills,List<Jogador> jogador){
+        this.allkills=0;
+        this.gamenumber=gamenumber;
+        this.jogador=jogador;
+    }
+
+    public List<Jogador> getJogador() {
+        return jogador;
+    }
+
+    public int getAllkills() {
+        return allkills;
+    }
+
+    public void setAllkills(int allkills) {
+        this.allkills = allkills;
+    }
+
+    public String getGamenumber() {
+        return gamenumber;
+    }
+
+    public void setGamenumber(String gamenumber) {
+        this.gamenumber = gamenumber;
+    }
 }
